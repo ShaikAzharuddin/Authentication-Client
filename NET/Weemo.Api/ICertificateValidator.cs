2bcd6f126c1f2f2a3f6e7b6144055a2a69de7630
Sam Nazari <samnazari@users.noreply.github.com>
2015-03-12 10:54:07 -0700

    Update ICertificateValidator.cs

--- NET/Weemo.Api/ICertificateValidator.cs
+++ NET/Weemo.Api/ICertificateValidator.cs
@@ -12,7 +12,7 @@
         /// <summary>
         /// Validates whether certificate data being returned in a Response is valid
         /// </summary>
-        /// <param name="privateCertAuthority">The known Private Cert Authority (i.e. from the "weemo-ca.pem" file given to you by Weemo)</param>
+        /// <param name="privateCertAuthority">The known Private Cert Authority (i.e. from the "authCA.crt" file given to you by Weemo)</param>
         /// <param name="toBeValidated">The certificated data from the response</param>
         /// <returns>true if the certificate data is valid, otherwise false</returns>
         bool ValidateRemoteCertificate(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors);
